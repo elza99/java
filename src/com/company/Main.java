05c39d8fd95ec98dd7d5944b4d84e9481a34e6f8
elza99 <elikhadzhieva@seznam.cz>
2021-06-17 19:59:30 +0200

    Home task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    double cat = 3;
-	    double dog = 2;
-	    double multiply = cat / dog;
-        System.out.println(multiply);
+//	    double cat = 3;
+//	    double dog = 2;
+//	    double multiply = cat / dog;
+//        System.out.println(multiply);
+
+
+        /*
+            Написать калькулятор для целых чисел (+, -, *, /)
+            
+         */
     }
 }
