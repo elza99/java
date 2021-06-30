5433c3e7f1c4c7cc844300f9d6a391ce4768fc1c
elza99 <elikhadzhieva@seznam.cz>
2021-06-30 21:22:37 +0200

    calculator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,64 +1,35 @@
-package com.company;
+package com.company;    //calculator for Andrei
 import java.util.Scanner;
-
-import static java.lang.System.*;
-
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Enter first number");
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        System.out.println("Enter second number");
+        int number2 = sc.nextInt();
+        System.out.println("Enter operation");
+        String operation = sc.next();
 
-        Scanner scanner = new Scanner(in);
-
-        out.println("Enter the first number: ");
-        String number1Str = scanner.next();
-        Integer number1 = parseIntOrNull(number1Str);
-        if (number1 == null)
-        {
-            out.println("Incorrect number");
-            return;
+        if (operation.equals("+")) {
+            System.out.println(number1 + " " + "+" + " " + number2 + " = " +(number1+number2));
         }
 
-        out.println("Enter the second number: ");
-        String number2Str = scanner.next();
-        Integer number2 = parseIntOrNull(number2Str);
-        if (number2 == null)
-        {
-            out.println("Incorrect number");
-            return;
+        if (operation.equals("-")) {
+            System.out.println(number1 + " " + "-" + " " + number2 + " = " +(number1-number2));
         }
 
-        out.println("Enter operation: ");
-
-        String str = scanner.next();
-        if (str.length() != 1) {
-            out.println("Incorrect operation");
-            return;
+        if (operation.equals("*")) {
+            System.out.println(number1 + " " + "*" + " " + number2 + " = " +(number1*number2));
         }
 
-        Character operation = str.charAt(0);
-        if (operation == '+') {
-            out.println(number1 + " " + operation + " " + number2 + " = " + (number1 + number2));
-        } else if (operation == '-') {
-            out.println(number1 + " " + operation + " " + number2 + " = " + (number1 - number2));
-        } else if (operation == '/') {
-            if (number2 == 0)
-            {
-                out.println("/ by zero is forbidden in this world");
-                return;
-            }
-            out.println(number1 + " " + operation + " " + number2 + " = " + (number1 / number2));
-        } else if (operation == '*') {
-            out.println(number1 + " " + operation + " " + number2 + " = " + (number1 * number2));
-        } else {
-            out.println("Incorrect operation");
+        if (operation.equals("/")) {
+            System.out.println(number1 + " " + "/" + " " + number2 + " = " +(number1/number2));
         }
-    }
 
-    public static Integer parseIntOrNull(String value) {
-        try {
-            return Integer.parseInt(value);
-        } catch (NumberFormatException e) {
-            return null;
-        }
+
+
+
+
     }
 }
