cddfc5fdc8944be531a1706133d587a80e770bea
elza99 <elikhadzhieva@seznam.cz>
2021-07-07 21:10:13 +0200

    07.07

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,55 @@
+/*
 package com.company;    //calculator for Andrei
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Enter first number");
-        Scanner sc = new Scanner(System.in);
-        int number1 = sc.nextInt();
-        System.out.println("Enter second number");
-        int number2 = sc.nextInt();
-        System.out.println("Enter operation");
-        String operation = sc.next();
+    public static void main(String[] args)  throws IOException {
+//        System.out.println("Enter first, second number and operation");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String lines = br.readLine();
+//        String[] strs = lines.trim().split("\\s+");
+//        int number1 = Integer.parseInt(strs[0]);
+//        System.out.println(number1);
+//        int number2 = Integer.parseInt(strs[1]);
+//        System.out.println(number2);
+//        String operation = strs[2];
+//
+//        if (operation.equals("+")) {
+//            System.out.println(number1 + " " + "+" + " " + number2 + " = " + (number1 + number2));
+//        } else if (operation.equals("-")) {
+//            System.out.println(number1 + " " + "-" + " " + number2 + " = " +(number1-number2));
+//        } else if (operation.equals("*")) {
+//            System.out.println(number1 + " " + "*" + " " + number2 + " = " +(number1*number2));
+//        } else if (operation.equals("/")) {
+//            System.out.println(number1 + " " + "/" + " " + number2 + " = " +(number1/number2));
+//        }
+//        else {
+//            System.out.println("Good bye, it won't work");
+//        }
 
-        if (operation.equals("+")) {
-            System.out.println(number1 + " " + "+" + " " + number2 + " = " + (number1 + number2));
-        } else if (operation.equals("-")) {
-            System.out.println(number1 + " " + "-" + " " + number2 + " = " +(number1-number2));
-        } else if (operation.equals("*")) {
-            System.out.println(number1 + " " + "*" + " " + number2 + " = " +(number1*number2));
-        } else if (operation.equals("/")) {
-            System.out.println(number1 + " " + "/" + " " + number2 + " = " +(number1/number2));
+        int[] numbers = {1,3,2,-54,0,1243,8,10};
+        for(int i =0; i < numbers.length; i++) {
+            if(( numbers[i] %2) ==0) {
+                System.out.println(numbers[i] + " is even" );
+            }else {
+                System.out.println(numbers[i] + " is odd");
+            }
         }
-        else {
-            System.out.println("Good bye, it won't work");
-        }
 
+        // if (x%2) == 0
 
+        // homework : считать строку чисел с ввода и потом найти сумму всех четных и нечетных чисел
+
+        // Input:
+        // 1 3 5 2 4 6              0 0 0 0 0 1
+        // Output:
+        // 9 12                     1 0
 
 
 
     }
 }
+*/
